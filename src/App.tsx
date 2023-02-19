4bcc75e868a6fa8f6ee3744c50dc4902699c4aa1
Matchobas <maalexandria@hotmail.com>
2023-02-18 22:20:25 -0300

    Adding tailwind config

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,9 @@
+import './styles/global.css';
+
 export function App() {
-  return <div>Roullete</div>
+  return (
+    <div className='w-full h-screen font-extrabold text-8xl flex items-center justify-center'>
+      Roullete
+    </div>
+  )
 }
