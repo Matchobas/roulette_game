099726d7b3cfafbf0658884a23c0cf3f1d76b131
Matchobas <maalexandria@hotmail.com>
2023-02-19 23:41:33 -0300

    Creating add option form no css

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,35 @@
+import { FormEvent, useState } from 'react';
 import './styles/global.css';
 
 export function App() {
+  const [option, setOption] = useState<string>('');
+  const [rouletteOptions, setRouletteOptions] = useState<string[]>([]);
+
+  function handleAddOption(event: FormEvent) {
+    event.preventDefault();
+
+    if (option) {
+      setRouletteOptions([...rouletteOptions, option]);
+    }
+  }
+
   return (
-    <div className='w-full h-screen font-extrabold text-8xl flex items-center justify-center'>
-      Roullete
-    </div>
+    <>
+      <form
+        onSubmit={(event) => {handleAddOption(event)}}
+      >
+        <input
+          type={'text'}
+          placeholder={'Add your roulette option'}
+          onChange={(e) => setOption(e.target.value)}
+        />
+        <button type='submit'>
+          Adicionar
+        </button>
+      </form>
+      <div className='w-full h-screen font-extrabold flex items-center justify-center'>
+        {rouletteOptions}
+      </div>
+    </>
   )
 }
