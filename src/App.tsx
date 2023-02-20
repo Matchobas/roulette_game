102ccbf5f2d6733d41daa0a2a04304580cd58454
Matchobas <maalexandria@hotmail.com>
2023-02-19 23:56:02 -0300

    First CSS added to tags

--- src/App.tsx
+++ src/App.tsx
@@ -14,22 +14,24 @@ export function App() {
   }
 
   return (
-    <>
+    <div className='w-full h-screen flex items-center justify-center bg-zinc-700'>
       <form
+        className='flex flex-col items-center justify-center gap-3'
         onSubmit={(event) => {handleAddOption(event)}}
       >
         <input
           type={'text'}
-          placeholder={'Add your roulette option'}
+          placeholder={'Add a new roulette option'}
           onChange={(e) => setOption(e.target.value)}
+          className='p-2 ring-4 ring-blue-800'
         />
-        <button type='submit'>
+        <button type='submit' className='w-40 h-10 bg-zinc-500 text-white rounded-lg hover:bg-zinc-600 transition-colors'>
           Adicionar
         </button>
       </form>
-      <div className='w-full h-screen font-extrabold flex items-center justify-center'>
+      <div className='font-extrabold'>
         {rouletteOptions}
       </div>
-    </>
+    </div>
   )
 }
