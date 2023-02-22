75cde030d854e68c043346b6c8bd65f52b0301ef
Matchobas <maalexandria@hotmail.com>
2023-02-21 21:16:37 -0300

    Add percentage info to form

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
 import { FormEvent, useState } from 'react';
 import './styles/global.css';
 
+interface RoulleteOption {
+  title: string;
+  percentage: number;
+}
+
 export function App() {
   const [option, setOption] = useState<string>('');
-  const [rouletteOptions, setRouletteOptions] = useState<string[]>([]);
+  const [percentage, setPercentage] = useState<number>(100);
+  const [rouletteOptions, setRouletteOptions] = useState<RoulleteOption[]>([]);
 
   function handleAddOption(event: FormEvent) {
     event.preventDefault();
 
     if (option) {
-      setRouletteOptions([...rouletteOptions, option]);
+      setRouletteOptions([...rouletteOptions, { title: option, percentage }]);
     }
   }
 
@@ -18,9 +24,14 @@ export function App() {
       <div className='w-1/2 flex flex-col items-center justify-center gap-2 mr-4'>
         {rouletteOptions.map((option, index) => {
           return (
-            <span key={`${option}-${index}`} className='font-extrabold text-white'>
-              {option}
-            </span>
+            <div key={`${option.title}-${index}`}>
+              <span className='font-extrabold text-white'>
+                {`${option.percentage} `}
+              </span>
+              <span className='font-extrabold text-white'>
+                {option.title}
+              </span>
+            </div>
           )
         })}
       </div>
@@ -28,12 +39,20 @@ export function App() {
         className='flex flex-col items-center justify-center gap-3'
         onSubmit={(event) => {handleAddOption(event)}}
       >
-        <input
-          type={'text'}
-          placeholder={'Add a new roulette option'}
-          onChange={(e) => setOption(e.target.value)}
-          className='p-2 ring-4 ring-blue-800'
-        />
+        <div>
+          <input
+            type={'text'}
+            defaultValue={percentage}
+            onChange={(e) => setPercentage(Number(e.target.value))}
+            className='w-12 p-2 ring-4 ring-blue-800 mr-2'
+          />
+          <input
+            type={'text'}
+            placeholder={'Add a new roulette option'}
+            onChange={(e) => setOption(e.target.value)}
+            className='p-2 ring-4 ring-blue-800'
+          />
+        </div>
         <button type='submit' className='w-40 h-10 bg-zinc-500 text-white rounded-lg hover:bg-zinc-600 transition-colors'>
           Adicionar
         </button>
