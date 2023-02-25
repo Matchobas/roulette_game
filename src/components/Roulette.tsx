9e6917fddc87b8a0cc7daa6fd33298ab6797c0c2
Matchobas <maalexandria@hotmail.com>
2023-02-25 11:04:58 -0300

    Testing with canvas arc and fill options

--- src/components/Roulette.tsx
+++ src/components/Roulette.tsx
@@ -4,16 +4,24 @@ export function Roulette() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
   useEffect(() => {
-    const canvas = canvasRef.current;
-    const ctx = canvas!.getContext("2d");
+    if (canvasRef.current) {
+      const canvas = canvasRef.current;
+      const ctx = canvas.getContext("2d");
 
-    const centerX = canvas!.width / 2;
-    const centerY = canvas!.height / 2;
-    const radius = 200;
+      const centerX = canvas.width / 2;
+      const centerY = canvas.height / 2;
+      const radius = 240;
 
-    ctx?.beginPath();
-    ctx?.arc(centerX, centerY, radius, 0, 2 * Math.PI);
-    ctx?.stroke();
+      if (ctx) {
+        ctx.beginPath();
+        ctx.moveTo(centerX, centerY);
+        ctx.arc(centerX, centerY, radius, 0, 0.3 * Math.PI);
+        ctx.closePath();
+
+        ctx.fillStyle = "#FFFFFF";
+        ctx.fill();
+      }
+    }
   }, []);
 
   return (
