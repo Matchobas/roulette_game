cf3f1b45518e28b64f9a57affb430a1a0edbf73c
Matchobas <maalexandria@hotmail.com>
2023-02-26 00:02:26 -0300

    More tests with angle and texts

--- src/components/Roulette.tsx
+++ src/components/Roulette.tsx
@@ -10,34 +10,40 @@ export function Roulette() {
 
       const centerX = canvas.width / 2;
       const centerY = canvas.height / 2;
-      const radius = 240;
+      const radius = 200;
+      const startAngle = 0;
+      const endAngle = (3 * Math.PI) / 4;
 
       if (ctx) {
         ctx.beginPath();
         ctx.moveTo(centerX, centerY);
-        ctx.arc(centerX, centerY, radius, 0, 0.3 * Math.PI);
+        ctx.arc(centerX, centerY, radius, startAngle, endAngle);
         ctx.closePath();
 
         ctx.fillStyle = "#FFFFFF";
         ctx.fill();
 
         ctx.fillStyle = "#000000";
-        ctx.font = "24px Arial";
+        ctx.font = "20px Arial";
         ctx.textAlign = "center";
         ctx.textBaseline = "middle";
         ctx.strokeStyle = "none";
 
-        const angleToRotate = 0;
-        const angleInRadians = angleToRotate * Math.PI / 180;
-
+        const angleInDegrees = 135 / 2;
+        const angleInRadians = angleInDegrees * Math.PI / 180;
         ctx.rotate(angleInRadians);
-        ctx.fillText("Hello, World!", centerX, centerY);
+
+        // Calculate the position of the text based on the angle and radius
+        const x = radius * Math.cos(endAngle - angleInRadians / 2) + centerX;
+        const y = radius * Math.sin(endAngle - angleInRadians / 2) + centerY;
+
+        ctx.fillText("Hello, World!", centerX + x, -centerY + 90);
         ctx.rotate(-angleInRadians);
       }
     }
   }, []);
 
   return (
-    <canvas ref={canvasRef} width={500} height={500} />
+    <canvas ref={canvasRef} width={400} height={400} />
   )
 }
