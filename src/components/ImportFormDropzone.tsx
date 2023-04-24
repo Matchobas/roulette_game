b4d223d0611b1dda5cca6224c1aabf51689f089c
Matchobas <maalexandria@hotmail.com>
2023-04-24 14:51:44 -0300

    Dropzone styled and open close logic implemented

--- src/components/ImportFormDropzone.tsx
+++ src/components/ImportFormDropzone.tsx
@@ -12,24 +12,30 @@ export function ImportFormDropzone({ handleWheelOptions, isModalOpen }: ImportFo
   const [isDropzoneOpen, setIsDropzoneOpen] = useState(false);
   const { acceptedFiles, getRootProps, getInputProps } = useDropzone();
 
+  const file = acceptedFiles.map((file) => 
+    (
+      <p key={file.name}>{file.name}</p>
+    )
+  )
+
   useEffect(() => {
     setIsDropzoneOpen(false);
   }, [isModalOpen]);
 
   return (
     <>
-      <button onClick={() => setIsDropzoneOpen(true)}>
+      <button onClick={() => setIsDropzoneOpen(!isDropzoneOpen)}>
         <FileArrowDown size={20} weight="bold" className="text-white mr-4" />
       </button>
 
-      {/* <div className="fixed left-1/2 top-1/2 text-white text-xl transform -translate-x-1/2 -translate-y-1/2" >
-        <p>Testando div fixada</p>
-      </div> */}
-
       {isDropzoneOpen && (
-        <div {...getRootProps({className: 'fixed inset-0 top-1/2 left-1/2 text-white transform -translate-x-1/4'})}>
+        <div {...getRootProps({
+          className: 'fixed w-1/2 h-1/4 flex items-center justify-center border-2 border-gray-400 border-dashed bg-slate-600 inset-0 top-1/2 left-1/2 text-x text-white transform -translate-x-1/2 -translate-y-1/2'
+        })}>
           <input {...getInputProps()} />
-          <p>Drag 'n' drop some files here, or click to select files</p>
+          {file.length > 0 ? file : (
+            <p>Drag 'n' drop some files here, or click to select files</p>
+          )}
         </div>
       )}
     </>
