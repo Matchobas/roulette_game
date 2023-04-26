5d1e43e071c5ce729799ee08e6e8e4737ddc2ae4
Matchobas <maalexandria@hotmail.com>
2023-04-25 22:51:15 -0300

    Send options file button

--- src/components/ImportFormDropzone.tsx
+++ src/components/ImportFormDropzone.tsx
@@ -9,7 +9,7 @@ interface ImportFormDropzoneProps {
 }
 
 export function ImportFormDropzone({ handleWheelOptions, isModalOpen }: ImportFormDropzoneProps) {
-  const [isDropzoneOpen, setIsDropzoneOpen] = useState(false);
+  const [isDropzoneOpen, setIsDropzoneOpen] = useState(true);
   const { acceptedFiles, getRootProps, getInputProps } = useDropzone();
 
   const file = acceptedFiles.map((file) => 
@@ -19,7 +19,7 @@ export function ImportFormDropzone({ handleWheelOptions, isModalOpen }: ImportFo
   )
 
   useEffect(() => {
-    setIsDropzoneOpen(false);
+    // setIsDropzoneOpen(false);
   }, [isModalOpen]);
 
   return (
@@ -30,12 +30,13 @@ export function ImportFormDropzone({ handleWheelOptions, isModalOpen }: ImportFo
 
       {isDropzoneOpen && (
         <div {...getRootProps({
-          className: 'fixed w-1/2 h-1/4 flex items-center justify-center border-2 border-gray-400 border-dashed bg-slate-600 inset-0 top-1/2 left-1/2 text-x text-white transform -translate-x-1/2 -translate-y-1/2'
+          className: 'fixed w-1/2 h-1/4 flex flex-col items-center justify-center border-2 border-gray-400 border-dashed bg-slate-600 inset-0 top-1/2 left-1/2 text-x text-white transform -translate-x-1/2 -translate-y-1/2'
         })}>
           <input {...getInputProps()} />
           {file.length > 0 ? file : (
             <p>Drag 'n' drop some files here, or click to select files</p>
           )}
+          <button disabled={true} className="px-2 bg-zinc-400 text-white rounded-md mt-6 hover:bg-zinc-500 transition-colors">Load options</button>
         </div>
       )}
     </>
