42e5a054621e14023679fa93c271bf251247d361
Matchobas <maalexandria@hotmail.com>
2023-04-26 11:40:06 -0300

    Configuring drag n drop space

--- src/components/ImportFormDropzone.tsx
+++ src/components/ImportFormDropzone.tsx
@@ -10,11 +10,16 @@ interface ImportFormDropzoneProps {
 
 export function ImportFormDropzone({ handleWheelOptions, isModalOpen }: ImportFormDropzoneProps) {
   const [isDropzoneOpen, setIsDropzoneOpen] = useState(true);
-  const { acceptedFiles, getRootProps, getInputProps } = useDropzone();
+  const { acceptedFiles, getRootProps, getInputProps } = useDropzone({
+    accept: {
+      'text/csv': ['.csv']
+    },
+    maxFiles: 1
+  });
 
   const file = acceptedFiles.map((file) => 
     (
-      <p key={file.name}>{file.name}</p>
+      <p className="text-white text-lg" key={file.name}>{file.name}</p>
     )
   )
 
@@ -25,18 +30,26 @@ export function ImportFormDropzone({ handleWheelOptions, isModalOpen }: ImportFo
   return (
     <>
       <button onClick={() => setIsDropzoneOpen(!isDropzoneOpen)}>
-        <FileArrowDown size={20} weight="bold" className="text-white mr-4" />
+        <FileArrowDown size={20} weight="bold" className="text-white mr-4 text-" />
       </button>
 
       {isDropzoneOpen && (
         <div {...getRootProps({
-          className: 'fixed w-1/2 h-1/4 flex flex-col items-center justify-center border-2 border-gray-400 border-dashed bg-slate-600 inset-0 top-1/2 left-1/2 text-x text-white transform -translate-x-1/2 -translate-y-1/2'
+          className: 'fixed w-1/2 h-1/4 flex flex-col items-center justify-center border-2 border-gray-400 border-dashed bg-slate-600 inset-0 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'
         })}>
           <input {...getInputProps()} />
           {file.length > 0 ? file : (
-            <p>Drag 'n' drop some files here, or click to select files</p>
+            <p className="text-white text-lg">Drag 'n' drop a csv file here, or click to select one</p>
           )}
-          <button disabled={true} className="px-2 bg-zinc-400 text-white rounded-md mt-6 hover:bg-zinc-500 transition-colors">Load options</button>
+          
+          {acceptedFiles.length > 0 && (
+            <button
+              onClick={() => console.log("Clicked")}
+              className="px-2 bg-zinc-400 text-white rounded-md mt-6 hover:bg-zinc-500 transition-colors opacity-100"
+            >
+              Load options
+            </button>
+          )}
         </div>
       )}
     </>
