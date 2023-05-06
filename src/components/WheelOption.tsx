1a336f3a5798c01458918014115df63ece46b769
Matchobas <maalexandria@hotmail.com>
2023-05-06 12:21:17 -0300

    Eslint configuration added

--- src/components/WheelOption.tsx
+++ src/components/WheelOption.tsx
@@ -1,5 +1,6 @@
-import { X } from "phosphor-react"
-import { WheelOptionModel } from "../model/WheelOptionModel"
+import { X } from "phosphor-react";
+
+import { WheelOptionModel } from "../model/WheelOptionModel";
 
 interface WheelOptionProps {
   option: WheelOptionModel;
@@ -8,47 +9,48 @@ interface WheelOptionProps {
   index: number;
 }
 
-export function WheelOption({ option, updateOption, removeOption, index }: WheelOptionProps) {
+export function WheelOption({
+  option,
+  updateOption,
+  removeOption,
+  index
+}: WheelOptionProps) {
   return (
-    <div className='w-[98%] min-h-[37px] flex justify-between items-center p-2 bg-gray-900 rounded-md'>
+    <div className="w-[98%] min-h-[37px] flex justify-between items-center p-2 bg-gray-900 rounded-md">
       <div className="flex items-center">
         <input
-          type={'text'}
+          type={"text"}
           defaultValue={option.percentage}
           value={option.percentage}
           onChange={(e) => {
             const valueAsNumber = Number(e.target.value);
-            if (!isNaN(valueAsNumber)) {           
+            if (!Number.isNaN(valueAsNumber)) {
               updateOption(valueAsNumber, option.title, index);
             } else {
               updateOption(option.percentage, option.title, index);
             }
           }}
-          className='font-extrabold text-white w-12 bg-gray-900'
+          className="font-extrabold text-white w-12 bg-gray-900"
         />
         <input
-          type={'text'}
+          type={"text"}
           defaultValue={option.title}
           value={option.title}
           onChange={(e) => {
             const newTitle = e.target.value.toString();
             updateOption(option.percentage, newTitle, index);
           }}
-          // onBlur={(e) => {
-          //   const newTitle = e.target.value.toString();
-          //   updateOption(option.percentage, newTitle, index);
-          // }}
-          className='font-extrabold w-full text-white bg-gray-900'
+          className="font-extrabold w-full text-white bg-gray-900"
         />
       </div>
-      <button 
-        type='button' 
-        onClick={() => { 
-          removeOption(index); 
+      <button
+        type="button"
+        onClick={() => {
+          removeOption(index);
         }}
       >
-        <X size={16} color="white"/>
+        <X size={16} color="white" />
       </button>
     </div>
-  )
+  );
 }
