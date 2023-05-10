c6ea3d7c8859cafe4703d3c8b6b5f39be799de12
Matchobas <maalexandria@hotmail.com>
2023-05-10 11:37:54 -0300

    Going back to gh-pages deploy method

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/wheel_of_fortune_game"
+  base: "/wheel_of_fortune_game/"
 });
